8901c71c11ca84fe60fe6dfe6acaa42006150fd3
Sebastian Gehaxelt <github@gehaxelt.in>
2019-10-03 06:06:55 +0200

    Check for reserved keywords in randomly generated string

--- exceptions.junkcoderandom.class.php
+++ exceptions.junkcoderandom.class.php
@@ -3,4 +3,7 @@ if(!class_exists('NotAnIntegerException'))
 {
 	class NotAnIntegerException extends Exception{}
 }
+if(!class_exists('IllegalNameException')) {
+    class IllegalNameException extends Exception{}
+}
 ?>
--- junkcoderandom.class.php
+++ junkcoderandom.class.php
@@ -30,12 +30,16 @@ class JunkCodeRandom {
 		if(!is_int($length))
 			throw new NotAnIntegerException('$length is not an integer');
 		
-		$charset= 'abcdefghijklmnopqrstuvwxyz';
+		$charset= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 		$randomString='';
 		
 		for($i=$length;$i>0;$i--)
 			$randomString.=$charset[mt_rand(0,25)];
 		
+		// Check that the variables do not contain C++ (reserved) keywords
+		if (preg_match('/(__has_cpp_attribute|__has_include|alignas|alignof|and|and_eq|asm|atomic_cancel|atomic_commit|atomic_noexcept|auto|bitand|bitor|bool|break|case|catch|char|char16_t|char32_t|char8_t|class|co_await|co_return|co_yield|compl|concept|const|const_cast|consteval|constexpr|constinit|continue|decltype|default|define|defined|delete|do|double|dynamic_cast|elif|else|else|endif|enum|error|explicit|export|extern|false|final|float|for|friend|goto|if|if|ifdef|ifndef|import|include|inline|int|line|long|module|mutable|namespace|new|noexcept|not|not_eq|nullptr|operator|or|or_eq|override|pragma|private|protected|public|reflexpr|register|reinterpret_cast|requires|return|short|signed|sizeof|static|static_assert|static_cast|struct|switch|synchronized|template|this|thread_local|throw|transaction_safe|transaction_safe_dynamic|true|try|typedef|typeid|typename|undef|union|unsigned|using|virtual|void|volatile|wchar_t|while|xor|xor_eq)/', $randomString)) {
+			throw new IllegalNameException("Reserved keyword in randomString ${randomString}");
+		}
 		return $randomString;
 	}
 }
